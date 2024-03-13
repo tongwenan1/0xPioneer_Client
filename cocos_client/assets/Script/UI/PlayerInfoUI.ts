807af57fac5373a41d9c6ce3c6489ef2540e119d
xxCary0113 <xx1076577400@gmail.com>
2024-03-13 23:23:12 +0800

    changed: - player ui

--- cocos_client/assets/Script/UI/PlayerInfoUI.ts
+++ cocos_client/assets/Script/UI/PlayerInfoUI.ts
@@ -7,9 +7,13 @@ import ItemMgr from '../Manger/ItemMgr';
 import { AudioMgr } from '../Basic/AudioMgr';
 import LanMgr from '../Manger/LanMgr';
 import EventMgr from '../Manger/EventMgr';
-import { EventName } from '../Const/ConstDefine';
+import { EventName, ItemConfigType } from '../Const/ConstDefine';
 import SettlementMgr from '../Manger/SettlementMgr';
 import { SettlementView } from './View/SettlementView';
+import ArtifactData from '../Model/ArtifactData';
+import ItemData from '../Model/ItemData';
+import { ArtifactItem } from './ArtifactItem';
+import { BackpackItem } from './BackpackItem';
 const { ccclass, property } = _decorator;
 
 @ccclass('PlayerInfoUI')
@@ -27,7 +31,9 @@ export class PlayerInfoUI extends PopUpUI implements UserInfoEvent {
     private _changeNameView: Node = null;
     private _nextLevelView: Node = null;
     private _rewardItem: Node = null;
+    private _artifactItem: Node = null;
     private _showRewardItems: Node[] = [];
+    private _showArtifactItems: Node[] = [];
 
     private _settleSelectItem: Node = null;
     private _settleUseSelectItems: Node[] = [];
@@ -67,8 +73,11 @@ export class PlayerInfoUI extends PopUpUI implements UserInfoEvent {
         this._changeNameView.active = false;
 
         this._nextLevelView = this.node.getChildByName("NextLevelContent");
-        this._rewardItem = this._nextLevelView.getChildByPath("NextLevelInfo/RewardContent/Rewards/Content/Item")
+        this._rewardItem = this._nextLevelView.getChildByPath("NextLevelInfo/RewardContent/Rewards/Content/BackpackItem")
         this._rewardItem.active = false;
+        this._artifactItem = this._nextLevelView.getChildByPath("NextLevelInfo/RewardContent/Rewards/Content/ArtifactItem")
+        this._artifactItem.active = false;
+
         this._nextLevelView.active = false;
 
         this._settleSelectItem = summaryView.getChildByPath("SettlementList/view/content/Item");
@@ -262,8 +271,8 @@ export class PlayerInfoUI extends PopUpUI implements UserInfoEvent {
     private async _refreshNextLevelView() {
         const nextLevel = UserInfoMgr.Instance.level + 1;
         // useLanMgr 
-        // this._nextLevelView.getChildByPath("NextLevelInfo/NextLevel").getComponent(Label).string = LanMgr.Instance.getLanById("107549") + "  " + nextLevel;
-        this._nextLevelView.getChildByPath("NextLevelInfo/NextLevel").getComponent(Label).string = "Civilization Level  " + nextLevel;
+        // this._nextLevelView.getChildByPath("NextLevelInfo/UserCivilizationLv/Title").getComponent(Label).string = LanMgr.Instance.getLanById("107549")
+        this._nextLevelView.getChildByPath("NextLevelInfo/UserCivilizationLv/NextLevel").getComponent(Label).string = nextLevel.toString();
 
         const rewardView = this._nextLevelView.getChildByPath("NextLevelInfo/RewardContent");
         const maxTip = this._nextLevelView.getChildByPath("NextLevelInfo/MaxTip");
@@ -275,14 +284,14 @@ export class PlayerInfoUI extends PopUpUI implements UserInfoEvent {
             const content = rewardView;
             content.getChildByName("CityVersion").active = levelConfig.city_vision != null && levelConfig.city_vision > 0;
             // useLanMgr 
-            // content.getChildByName("CityVersion").getComponent(Label).string = "> "+ LanMgr.Instance.getLanById("107549") + " + " + levelConfig.city_vision;
-            content.getChildByName("CityVersion").getComponent(Label).string = "> City Vision Expand + " + levelConfig.city_vision;
+            // content.getChildByPath("CityVersion/Content/Title").getComponent(Label).string = LanMgr.Instance.getLanById("107549");
+            content.getChildByPath("CityVersion/Value").getComponent(Label).string = "+" + levelConfig.city_vision;
 
             if (levelConfig.extra_res != null && levelConfig.extra_res > 0) {
                 content.getChildByName("ResGetRateUp").active = true;
-                // useLanMgr 
-                // content.getChildByName("ResGetRateUp").getComponent(Label).string = "> " + LanMgr.Instance.getLanById("107549") + " + " + levelConfig.extra_res * 100 + "%!";
-                content.getChildByName("ResGetRateUp").getComponent(Label).string = "> Resources Gained + " + levelConfig.extra_res * 100 + "%!";
+                // useLanMgr
+                // content.getChildByPath("ResGetRateUp/Content/Title").getComponent(Label).string = LanMgr.Instance.getLanById("107549");
+                content.getChildByPath("ResGetRateUp/Value").getComponent(Label).string = "+" + levelConfig.extra_res * 100 + "%";
             } else {
                 content.getChildByName("ResGetRateUp").active = false;
             }
@@ -290,31 +299,41 @@ export class PlayerInfoUI extends PopUpUI implements UserInfoEvent {
             if (levelConfig.hp_max != null && levelConfig.hp_max > 0) {
                 content.getChildByName("GetHpMax").active = true;
                 // useLanMgr 
-                // content.getChildByName("GetHpMax").getComponent(Label).string = "> " + LanMgr.Instance.getLanById("107549") + " + " + levelConfig.hp_max + "!";
-                content.getChildByName("GetHpMax").getComponent(Label).string = "> HP Max + " + levelConfig.hp_max + "!";
+                // content.getChildByPath("GetHpMax/Content/Title").getComponent(Label).string = LanMgr.Instance.getLanById("107549");
+                content.getChildByPath("GetHpMax/Value").getComponent(Label).string = "+" + levelConfig.hp_max;
             } else {
                 content.getChildByName("GetHpMax").active = false;
             }
 
             if (levelConfig.reward != null && levelConfig.reward.length > 0) {
                 content.getChildByName("Rewards").active = true;
-                for (const item of this._showRewardItems) {
-                    item.destroy();
-                }
-                this._showRewardItems = [];
-                for (const data of levelConfig.reward) {
-                    if (data.length == 3) {
-                        const type = data[0];
-                        const id = data[1];
-                        const num = data[2];
+            for (const item of [...this._showRewardItems, ...this._showArtifactItems]) {
+                item.destroy();
+            }
+            this._showRewardItems = [];
+            this._showArtifactItems = [];
+            for (const data of levelConfig.reward) {
+                if (data.length == 3) {
+                    const type = data[0];
+                    const id = data[1];
+                    const num = data[2];
+                    if (type == ItemConfigType.Item) {
                         const view = instantiate(this._rewardItem);
                         view.active = true;
-                        view.getChildByName("Icon").getComponent(Sprite).spriteFrame = await ItemMgr.Instance.getItemIcon(ItemMgr.Instance.getItemConf(id).icon);
-                        view.getChildByName("Num").getComponent(Label).string = "x" + num;
+                        view.getComponent(BackpackItem).refreshUI(new ItemData(id, num));
+                        view.getChildByName("Count").getComponent(Label).string = num;
                         view.setParent(content.getChildByPath("Rewards/Content"));
                         this._showRewardItems.push(view);
+                    } else if (type == ItemConfigType.Artifact) {
+                        const view = instantiate(this._artifactItem);
+                        view.active = true;
+                        view.getComponent(ArtifactItem).refreshUI(new ArtifactData(id, num));
+                        view.getChildByName("Count").getComponent(Label).string = num;
+                        view.setParent(content.getChildByPath("Rewards/Content"));
+                        this._showArtifactItems.push(view);
                     }
                 }
+            }
                 content.getChildByPath("Rewards/Content").getComponent(Layout).updateLayout();
             } else {
                 content.getChildByName("Rewards").active = false;
@@ -410,7 +429,7 @@ export class PlayerInfoUI extends PopUpUI implements UserInfoEvent {
     private onTapLangSelectShow() {
         this._langSelectView.active = true;
         this.node.getChildByPath("Content/tabContents/SettingsContent/LanguageMenu/LanguageBtn/Arrow").angle = 180;
-        
+
         this._langSelectView.getChildByPath("View/Content/English/ImgScreenSelect").active = this._selectLang == "eng";
         this._langSelectView.getChildByPath("View/Content/Chinese/ImgScreenSelect").active = this._selectLang == "cn";
     }
