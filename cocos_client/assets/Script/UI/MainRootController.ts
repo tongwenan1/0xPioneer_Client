3029c2c525c578f2c7a46d2024a8d0cddfbc24e9
xxCary0113 <xx1076577400@gmail.com>
2024-03-18 15:46:18 +0800

    added： -popUI mask  - rookieguide force hide switch

--- cocos_client/assets/Script/UI/MainRootController.ts
+++ cocos_client/assets/Script/UI/MainRootController.ts
@@ -1,4 +1,4 @@
-import { Component, _decorator } from "cc";
+import { CCBoolean, Component, _decorator } from "cc";
 import UserInfoMgr from "../Manger/UserInfoMgr";
 
 const { ccclass, property } = _decorator;
@@ -6,8 +6,15 @@ const { ccclass, property } = _decorator;
 
 @ccclass('MainRootController')
 export class MainRootController extends Component {
+    @property(CCBoolean)
+    private canShowRookieGuide: boolean = true;
+
     protected onLoad(): void {
         const rookieGuideView = this.node.getChildByName("RookieGuide");
-        rookieGuideView.active = !UserInfoMgr.Instance.isFinishRookie;
+        if (this.canShowRookieGuide) {
+            rookieGuideView.active = !UserInfoMgr.Instance.isFinishRookie;
+        } else {
+            rookieGuideView.active = false;
+        }
     }
 }
