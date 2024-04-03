ad5c4744a7d1371bd15c2ee95d35b8c004e35b75
xxCary0113 <xx1076577400@gmail.com>
2024-04-03 16:20:52 +0800

    fixed: - shake anim error

--- cocos_client/assets/Script/BasicView/ShakeAnim.ts
+++ cocos_client/assets/Script/BasicView/ShakeAnim.ts
@@ -7,30 +7,19 @@ export class ShakeAnim extends ViewController {
     @property(CCInteger)
     private shakeRange: number = 15;
 
-    private _anim = null;
-    protected viewDidAppear(): void {
-        super.viewDidAppear();
-
-        this._anim = tween()
-            .target(this.node)
-            .repeatForever(
-                tween().sequence(
-                    tween().by(0.65, { position: v3(0, this.shakeRange, 0) }),
-                    tween().by(0.35, { position: v3(0, -this.shakeRange, 0) }),
-                    tween().by(0.65, { position: v3(0, this.shakeRange, 0) }),
-                    tween().by(0.35, { position: v3(0, -this.shakeRange, 0) }),
-                    tween().delay(0)
-                )
-            ).start();
-    }
-
-    protected viewDidDisAppear(): void {
-        super.viewDidDisAppear();
-
-        if (this._anim != null) {
-            this._anim.stop();
-            this._anim = null;
-        }
+    protected viewDidStart(): void {
+        super.viewDidStart();
+        tween()
+        .target(this.node)
+        .repeatForever(
+            tween().sequence(
+                tween().by(0.65, { position: v3(0, this.shakeRange, 0) }),
+                tween().by(0.35, { position: v3(0, -this.shakeRange, 0) }),
+                tween().by(0.65, { position: v3(0, this.shakeRange, 0) }),
+                tween().by(0.35, { position: v3(0, -this.shakeRange, 0) }),
+                tween().delay(0)
+            )
+        ).start();
     }
 }
 
