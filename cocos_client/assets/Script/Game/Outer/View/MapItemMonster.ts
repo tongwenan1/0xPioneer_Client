deb03a3384ac5f88f62e07399d6a9a61b0fe5e19
xxCary0113 <xx1076577400@gmail.com>
2024-06-19 15:04:26 +0800

    fixed: - monster show error

--- cocos_client/assets/Script/Game/Outer/View/MapItemMonster.ts
+++ cocos_client/assets/Script/Game/Outer/View/MapItemMonster.ts
@@ -1,4 +1,4 @@
-import { _decorator, Component, Node, Vec2, Vec3, CCString, UITransform, Label, UIOpacity } from "cc";
+import { _decorator, Component, Node, Vec2, Vec3, CCString, UITransform, Label, UIOpacity, Layout } from "cc";
 import { LanMgr } from "../../../Utils/Global";
 import { MapPioneerLogicType, MapPioneerActionType, MapPioneerMoveDirection, MapPioneerObject, MapPioneerType } from "../../../Const/PioneerDefine";
 import { DataMgr } from "../../../Data/DataMgr";
@@ -10,12 +10,35 @@ const { ccclass, property } = _decorator;
 @ccclass("MapItemMonster")
 export class MapItemMonster extends Component {
     private _model: MapPioneerObject = null;
+    private _viewHeightMap: { [key: string]: number } = {
+        monster_a_1: 170,
+        monster_a_2: 170,
+        monster_a_3: 170,
+        monster_b_1: 240,
+        monster_b_2: 240,
+        monster_b_3: 240,
+        monster_c_1: 220,
+        monster_c_2: 220,
+        monster_c_3: 220,
+        monster_deathworm: 240,
+        monster_dhs: 190,
+        monster_sandspider: 190,
+        monster_scorpion: 320,
+        monster_spiderqueen: 340,
+        monster_teshark: 240,
+        monster_ths: 340,
+    };
 
     public refreshUI(model: MapPioneerObject) {
         this._model = model;
 
+        const infoView = this.node.getChildByPath("InfoView");
+
+        infoView.getChildByPath("Gap").getComponent(UITransform).height = this._viewHeightMap[this._model.animType];
+        infoView.getComponent(Layout).updateLayout();
+
         this._nameLabel.string = LanMgr.getLanById(model.name);
-        this.node.getChildByPath("Icon/Level").getComponent(Label).string = "Lv." + model.level;
+        infoView.getChildByPath("Content/Icon/Level").getComponent(Label).string = "Lv." + model.level;
 
         let moveCounting: boolean = false;
         if (model.logics.length > 0) {
@@ -28,39 +51,37 @@ export class MapItemMonster extends Component {
         this._moveCountLabel.node.active = moveCounting;
         this._moveCountLabel.node.active = false;
 
-        for (const type of this._monsterTypeNames) {
-            const view = this.node.getChildByPath("role/" + type);
-            view.active = type == model.animType;
-            if (view.active) {
-                this._currentShowMonster = view;
-                view.getChildByPath("idle").active = model.actionType == MapPioneerActionType.idle;
-                view.getChildByPath("walk_left").active =
-                    model.actionType == MapPioneerActionType.moving && model.moveDirection == MapPioneerMoveDirection.left;
-                view.getChildByPath("walk_right").active =
-                    model.actionType == MapPioneerActionType.moving && model.moveDirection == MapPioneerMoveDirection.right;
-                view.getChildByPath("walk_top").active = model.actionType == MapPioneerActionType.moving && model.moveDirection == MapPioneerMoveDirection.top;
-                view.getChildByPath("walk_bottom").active =
-                    model.actionType == MapPioneerActionType.moving && model.moveDirection == MapPioneerMoveDirection.bottom;
+        for (const child of this.node.getChildByPath("role").children) {
+            child.active = child.name == model.animType;
+            if (child.active) {
+                this._currentShowMonster = child;
+                child.getChildByPath("idle").active = model.actionType == MapPioneerActionType.idle;
+                // view.getChildByPath("walk_left").active =
+                //     model.actionType == MapPioneerActionType.moving && model.moveDirection == MapPioneerMoveDirection.left;
+                // view.getChildByPath("walk_right").active =
+                //     model.actionType == MapPioneerActionType.moving && model.moveDirection == MapPioneerMoveDirection.right;
+                // view.getChildByPath("walk_top").active = model.actionType == MapPioneerActionType.moving && model.moveDirection == MapPioneerMoveDirection.top;
+                // view.getChildByPath("walk_bottom").active =
+                //     model.actionType == MapPioneerActionType.moving && model.moveDirection == MapPioneerMoveDirection.bottom;
             }
         }
 
         this.node.active = model.actionType != MapPioneerActionType.fighting;
 
-        this.node.getChildByPath("Icon/Difficult").active = model.type == MapPioneerType.hred && model.level > DataMgr.s.artifact.getArtifactLevel();
+        infoView.getChildByPath("Content/Icon/Difficult").active = model.type == MapPioneerType.hred && model.level > DataMgr.s.artifact.getArtifactLevel();
     }
 
     public shadowMode() {
         this.node.getComponent(UIOpacity).opacity = 100;
-        for (const type of this._monsterTypeNames) {
-            const view = this.node.getChildByPath("role/" + type);
-            if (view.active) {
-                this._currentShowMonster = view;
-                view.getChildByPath("idle").active = true;
-                view.getChildByPath("idle/Monster_Shadow").active = false;
-                view.getChildByPath("walk_left").active = false;
-                view.getChildByPath("walk_right").active = false;
-                view.getChildByPath("walk_top").active = false;
-                view.getChildByPath("walk_bottom").active = false;
+        for (const child of this.node.getChildByPath("role").children) {
+            if (child.active) {
+                this._currentShowMonster = child;
+                child.getChildByPath("idle").active = true;
+                child.getChildByPath("idle/Monster_Shadow").active = false;
+                // view.getChildByPath("walk_left").active = false;
+                // view.getChildByPath("walk_right").active = false;
+                // view.getChildByPath("walk_top").active = false;
+                // view.getChildByPath("walk_bottom").active = false;
             }
         }
         this._nameLabel.node.active = false;
@@ -70,21 +91,8 @@ export class MapItemMonster extends Component {
     private _nameLabel: Label = null;
     private _moveCountLabel: Label = null;
     private _currentShowMonster: Node = null;
-    private _monsterTypeNames: string[] = [
-        "monster_a_1",
-        "monster_a_2",
-        "monster_a_3",
-
-        "monster_b_1",
-        "monster_b_2",
-        "monster_b_3",
-
-        "monster_c_1",
-        "monster_c_2",
-        "monster_c_3",
-    ];
     onLoad() {
-        this._nameLabel = this.node.getChildByPath("name").getComponent(Label);
+        this._nameLabel = this.node.getChildByPath("InfoView/Content/name").getComponent(Label);
         this._moveCountLabel = this.node.getChildByPath("MoveCountLabel").getComponent(Label);
 
         NotificationMgr.addListener(NotificationName.ARTIFACT_EQUIP_DID_CHANGE, this._onArtifactEquipChange, this);
